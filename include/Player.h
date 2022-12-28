c3c0dbca522bceda3d6371d3cdb25684dce2eefa
Van Sang Ho <vansang.hocorp@gmail.com>
2022-12-28 17:22:45 +0100

    added class Player

---
+++ include/Player.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "AttackGrid.h"
+#include "DefenseGrid.h"
+
+class Player {
+private:
+    AttackGrid atkGrid;
+    DefenseGrid defGrid;
+
+public:
+
+};
+
+#include "Player.hpp"
+
+#endif
---
+++ include/Player.hpp
@@ -0,0 +1,6 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "Player.h"
+
+#endif
