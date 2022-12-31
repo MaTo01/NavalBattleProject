32478477134808ee630877abb26358ee791752b4
Van Sang Ho <vansang.hocorp@gmail.com>
2022-12-31 18:42:06 +0100

    printGrid + other Grid functions

--- include/Grid.h
+++ include/Grid.h
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include "Position.h"
+#include "Ship.h"
 
 class Grid{
 private:
@@ -11,13 +12,30 @@ private:
 
 public:
     Grid();
-    ~Grid();
+    ~Grid() {}
 
     char getTile(unsigned int x, unsigned int y) const { return tiles[x][y]; }
-    char setTile(unsigned int x, unsigned int y, char c) { tiles[x][y] = c; }
+    void setTile(unsigned int x, unsigned int y, char c) { tiles[x][y] = c; }
 
-    std::ostream& printGrid(std::ostream& os);
+    bool placeShip(Ship& s, Position p);
+    bool moveShip(Ship& s, Position p);
+
+
+    void printGrid(std::ostream& os);
 };
+
+//Constants indicating extended ASCII characters used to display the grid
+static constexpr char cornerLU = 218;   // ┌
+static constexpr char cornerLD = 192;   // └
+static constexpr char cornerRU = 191;   // ┐
+static constexpr char cornerRD = 217;   // ┘
+static constexpr char lineH = 196;      // ─
+static constexpr char lineV = 179;      // │
+static constexpr char connectL = 180;   // ┤
+static constexpr char connectR = 195;   // ├
+static constexpr char connectU = 193;   // ┴
+static constexpr char connectD = 194;   // ┬
+static constexpr char connectLR = 197;  // ┼
 
 #include "Grid.hpp"
 
--- include/Grid.hpp
+++ include/Grid.hpp
@@ -5,8 +5,38 @@
 #include <iostream>
 #include "Grid.h"
 
-std::ostream& printGrid(std::ostream& os) {
-    
+Grid::Grid() {
+    for(int i=0; i<12; i++)
+        for(int j=0; j<12; j++)
+            tiles[i][j] = ' ';
+}
+
+void Grid::printGrid(std::ostream& os) {
+    os<<cornerLU;
+    for(int i=0; i<11; i++){
+        os<<lineH<<connectD;
+    }
+    os<<lineH<<cornerRU<<"\n";
+
+    for(int i=0; i<12; i++) {
+        for(int j=0; j<12; j++){
+            os<<lineV<<tiles[i][j];
+        }
+        os<<lineV<<"\n";
+        if(i<11) {
+            os<<connectR;
+            for(int j=0; j<11; j++){
+                os<<lineH<<connectLR;
+            }
+            os<<lineH<<connectL<<"\n";
+        }
+    }
+
+    os<<cornerLD;
+    for(int i=0; i<11; i++){
+        os<<lineH<<connectU;
+    }
+    os<<lineH<<cornerRD<<"\n";
 }
 
 #endif
