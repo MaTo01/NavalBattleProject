7d87f5b7f9cf0dd53fbcaac03fbb96f94a5bdc20
Van Sang Ho <vansang.hocorp@gmail.com>
2023-01-07 16:57:07 +0100

    unique_ptr in Player

--- include/Player.h
+++ include/Player.h
@@ -1,14 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <iostream>
+#include <vector>
+#include <memory>
 #include "Grid.h"
+#include "Ship.h"
+#include "Battleship.h"
+#include "SupportShip.h"
+#include "Submarine.h"
 
 class Player {
 private:
     Grid attackGrid;
     Grid defenseGrid;
+    std::vector<std::unique_ptr<Ship>> ships;
 
 public:
+    Player();
+    ~Player() = default;
 
 };
 
