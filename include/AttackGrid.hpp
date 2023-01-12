e663f41dbfbce47244b83b44bd59b32a4731aa8d
Van Sang Ho <vansang.hocorp@gmail.com>
2023-01-12 16:10:12 +0100

    exceptions

--- include/AttackGrid.hpp
+++ include/AttackGrid.hpp
@@ -7,25 +7,33 @@
 void AttackGrid::markHit(Position p) { 
     if(isPosValid(p)) {
         tiles_[p.getX()][p.getY()] = 'X'; 
-    }  
+    } else {
+        throw std::invalid_argument("Invalid position.");
+    }
 }
 
 void AttackGrid::markMiss(Position p) { 
     if(isPosValid(p)) {
         tiles_[p.getX()][p.getY()] = 'O'; 
-    }  
+    } else {
+        throw std::invalid_argument("Invalid position.");
+    }
 }
 
 void AttackGrid::markScan(Position p) { 
     if(isPosValid(p)) {
         tiles_[p.getX()][p.getY()] = 'Y'; 
-    }  
+    } else {
+        throw std::invalid_argument("Invalid position.");
+    }
 }
 
 bool AttackGrid::isAlreadyMarked(Position p) {
     if(isPosValid(p)) {
         return (tiles_[p.getX()][p.getY()] != ' ' && tiles_[p.getX()][p.getY()] != 'Y'); 
-    }  
+    } else {
+        throw std::invalid_argument("Invalid position.");
+    }
 }
 
 #endif
