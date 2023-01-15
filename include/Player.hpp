1b05c8c1ca64bc22d9c9a2ba90e66dc57c7bce48
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-15 14:19:17 +0100

    Update Player.hpp

--- include/Player.hpp
+++ include/Player.hpp
@@ -2,5 +2,76 @@
 #define PLAYER_HPP
 
 #include "Player.h"
+#include <iostream>
+
+void Player::placeShips(DefenseGrid* enemyDefenseGrid){
+    enemyDefenseGrid_ = enemyDefenseGrid;
+    for(int i=1; i<=nBattleships; i++){
+        try{
+          std::cout << "Inserire coordinate per la corazzata n. " << i << ":" << std::endl;
+        char bowX;
+        int bowY;
+        char sternX;
+        int sternY;
+        std::cin >> bowX >> bowY >> sternX >> sternY;
+        Position bowPos(Position::letterToNumber(bowX), bowY-1);
+        Position sternPos(Position::letterToNumber(sternX), sternY-1);
+        defenseGrid_->placeShip(std::unique_ptr<Ship>(new Battleship(bowPos, sternPos, attackGrid_)));  
+        }
+        catch(const std::invalid_argument& e){
+            std::cerr << e.what() << "Coordinate non valide.Riprovare" << '\n';
+            i--;
+        }
+        
+        
+    }
+    for(int i=1; i<=nSupportShips; i++){
+        try{
+            std::cout << "Inserire coordinate per la nave di supporto n. " << i << ":" << std::endl;
+            char bowX;
+            int bowY;
+            char sternX;
+            int sternY;
+            std::cin >> bowX >> bowY >> sternX >> sternY;
+            Position bowPos(Position::letterToNumber(bowX), bowY-1);
+            Position sternPos(Position::letterToNumber(sternX), sternY-1);
+            defenseGrid_->placeShip(std::unique_ptr<Ship>(new SupportShip(bowPos, sternPos, defenseGrid_)));
+        }
+        catch(const std::invalid_argument& e){
+            std::cerr << e.what() << "Coordinate non valide.Riprovare" << '\n';
+            i--;
+        }
+    }
+    for(int i=1; i<=nSubmarines; i++){
+        try{
+        std::cout << "Inserire coordinate per il sottomarino n. " << i << ":" << std::endl;
+        char bowX;
+        int bowY;
+        char sternX;
+        int sternY;
+        std::cin >> bowX >> bowY >> sternX >> sternY;
+        Position bowPos(Position::letterToNumber(bowX), bowY-1);
+        Position sternPos(Position::letterToNumber(sternX), sternY-1);
+        defenseGrid_->placeShip(std::unique_ptr<Ship> (new Submarine(bowPos, sternPos, attackGrid_, defenseGrid_)));
+        }
+        catch(const std::invalid_argument& e){
+            std::cerr << e.what() << "Coordinate non valide.Riprovare" << '\n';
+            i--;
+        }
+    }
+    defenseGrid_->printGrid(std::cout);
+}
+
+void Player::execute(){
+    char centerX;
+    int centerY;
+    char targetX;
+    int tragetY;
+    std::cout << "Selezionare nave e obiettivo:" << std::endl;
+    std::cin >> centerX >> centerY >> targetX >> tragetY;
+    Position center(Position::letterToNumber(centerX), centerY-1);
+    Position target(Position::letterToNumber(targetX), tragetY-1);
+    defenseGrid_->getShipByCenter(center)->action(target, enemyDefenseGrid_);
+}
 
 #endif
