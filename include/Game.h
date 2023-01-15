4889f8074dd1d69d16d090c0f0b6a0152174dd50
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-15 14:22:15 +0100

    Update Game.h

--- include/Game.h
+++ include/Game.h
@@ -6,11 +6,15 @@
 
 class Game{
 private:
+    Player player_;
+    Computer cpu_;
     static constexpr int maxTurns = 50;
-    static constexpr int maxBattleships = 3;
-    static constexpr int maxSupportShips = 3;
-    static constexpr int maxSubmarines = 2;
 public:
+    Game() = default;
+    ~Game() = default;
+
+    void setBattlefield();
+    void start();
 };
 
 #include "Game.hpp"
