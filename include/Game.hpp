a1252629a5bf2f41dccdd1612aa26c79d8986061
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-15 14:23:42 +0100

    Update Game.hpp

--- include/Game.hpp
+++ include/Game.hpp
@@ -2,5 +2,17 @@
 #define GAME_HPP
 
 #include "Game.h"
+#include <iostream>
+
+void Game::setBattlefield(){
+    Player p{};
+    Computer e{};
+    e.placeShips(p.getDefenseGrid());
+    p.placeShips(e.getDefenseGrid());
+}
+
+void Game::start(){
+    
+}
 
 #endif
