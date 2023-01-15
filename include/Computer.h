a205ca3f17dec6e5ccebe65fdc4799888f63eec8
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-15 18:05:26 +0100

    Update Computer.h

--- include/Computer.h
+++ include/Computer.h
@@ -18,25 +18,31 @@ private:
     static constexpr int nBattleships = 3;
     static constexpr int nSupportShips = 3;
     static constexpr int nSubmarines = 2;
+    static constexpr int nTotalShips = 8;
     static constexpr int BattleshipSize = 5;
     static constexpr int SupportShipSize = 3;
     static constexpr int SubmarineSize = 1;
     DefenseGrid *defenseGrid_;
     AttackGrid *attackGrid_;
+    DefenseGrid* enemyDefenseGrid_;
+
     std::ofstream logFile;
     
 public:
     //Constructor
-    Computer() : attackGrid_{new AttackGrid(rows, cols)}, defenseGrid_{new DefenseGrid(rows, cols)} {}
+    Computer() : attackGrid_{new AttackGrid(rows, cols)}, defenseGrid_{new DefenseGrid(rows, cols)} {srand(time(NULL));}
     //Destructor
     ~Computer() {
         logFile.close();
     }
     
     //Member functions
-    void placeShips(DefenseGrid* enemyDefenseGrid);
-    void performAction();
+    void placeShips();
+    void execute();
     DefenseGrid* getDefenseGrid() const {return defenseGrid_;}
+    void setEnemyDefenseGrid(DefenseGrid* enemyDefenseGrid){enemyDefenseGrid_=enemyDefenseGrid;}
+    void viewGrids();
+
 };
 
 #include "Computer.hpp"
