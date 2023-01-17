8579de67da07145ae1116774b7ea8500c3d2bb54
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-17 10:16:13 +0100

    Update Game.h

--- include/Game.h
+++ include/Game.h
@@ -2,19 +2,25 @@
 #define GAME_H
 
 #include "Player.h"
+#include "HumanPlayer.h"
 #include "Computer.h"
+#include <random>
 
 class Game{
 private:
-    Player player_;
-    Computer cpu_;
-    static constexpr int maxTurns = 50;
+    HumanPlayer humanPlayer_;
+    Computer cpu1_;
+    Computer cpu2_;
+    char secondPlayer_;
+    int counter_ = 0;
+    static constexpr int maxTurns_ = 50;
 public:
-    Game() {srand(time(NULL));}
+    Game(char secondPlayer) : secondPlayer_{secondPlayer} {srand(time(NULL));}
     ~Game() = default;
 
     void setBattlefield();
     void start();
+    void showWinner();
 };
 
 #include "Game.hpp"
