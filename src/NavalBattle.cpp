af7e8d65acdc61a1209d5dc192213f2ad7dc2b9b
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2023-01-18 21:14:44 +0100

    added args check

--- src/NavalBattle.cpp
+++ src/NavalBattle.cpp
@@ -4,10 +4,24 @@
 #include "Game.h"
 
 int main(int argc, char* argv[]){
-	Game game(*argv[1]);
-	game.setBattlefield();
-	game.start();
-    game.showWinner();
 
+	if(argc != 2){
+		std::cout << "Invalid argument list.\n";
+		return -1;
+	}
+
+	std::string argvToLower = "";
+	for (int x=0; x < strlen(argv[1]); x++)
+        argvToLower += tolower(argv[1][x]);
+	
+	if(argvToLower != "cc" && argvToLower != "pc"){
+		std::cout << "Invalid argument list.";
+	} else {
+		Game game(argvToLower[0]);
+		game.setBattlefield();
+		game.start();
+		game.showWinner();
+	}
+	
 	return 0;
 }
--- src/NavalBattleReplay.cpp
+++ src/NavalBattleReplay.cpp
@@ -6,12 +6,21 @@ int main(int argc, char* argv[]) {
 	
 	if(argc < 3 || argc > 4) {
 		std::cout << "Invalid argument list.\n";
+		return -1;
+	}
+
+	std::string argvToLower = "";
+	for (int x=0; x < strlen(argv[1]); x++)
+        argvToLower += tolower(argv[1][x]);
+
+	if(argvToLower != "v" && argvToLower != "f") {
+		std::cout << "Invalid argument list.\n";
 	} else {
 		std::string fileOut = "";
 		if(argc == 4) {
 			fileOut = std::string(argv[3]);
 		}
-		Game replay(*argv[1], argv[2], fileOut);
+		Game replay(argvToLower[0], argv[2], fileOut);
 		replay.setBattlefield();
 		replay.playReplay();
 	}
