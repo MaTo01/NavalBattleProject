3c3946ae188809c9eaef42d9125d9cc496e4ab95
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-18 22:11:25 +0100

    Update Game.h

--- include/Game.h
+++ include/Game.h
@@ -18,6 +18,8 @@
 
 class Game{
 private:
+    //Maximum of rounds allowed: beyond this limit
+    //match will end in a tie
     static constexpr int maxTurns_ = 50;
     static constexpr int rows_ = 12;
     static constexpr int cols_ = 12;
@@ -25,18 +27,26 @@ private:
     static constexpr int nSupportShips_ = 3;
     static constexpr int nSubmarines_ = 2;
 
+    //unique pointers to the pkayer, whether they are HumanPlayers o Computers
     std::unique_ptr<Player> player1;
     std::unique_ptr<Player> player2;
+    //char to identify which player is currently performing their action
     char mode_;
+    //output stream into which every command will be written
     std::ofstream logFileOut_;
+    //input stream which every command will be taken from
     std::ifstream logFileIn_;
     int turnCounter_ = 0;  
 
 public:
+    //Constructor
     Game(char mode, std::string logNameIn = "", std::string logNameOut = "");
+    //Distructor
     ~Game();
-
+    
+    //both players place their ships in their defense grids
     void setBattlefield();
+    //every palyer makes their move until either wins (or game ends in a tie)
     void start();
     void playReplay();
     void showWinner();
