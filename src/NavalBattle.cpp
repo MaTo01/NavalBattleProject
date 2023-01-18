19aac244203a3e3efd08e475d25f9f5d49d2e5bc
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2023-01-18 21:17:14 +0100

    spacing shit (sang'll love this)

--- src/NavalBattle.cpp
+++ src/NavalBattle.cpp
@@ -5,16 +5,17 @@
 
 int main(int argc, char* argv[]){
 
-	if(argc != 2){
+	if(argc != 2) {
 		std::cout << "Invalid argument list.\n";
 		return -1;
 	}
 
 	std::string argvToLower = "";
-	for (int x=0; x < strlen(argv[1]); x++)
-        argvToLower += tolower(argv[1][x]);
+	for (int x=0; x < strlen(argv[1]); x++) {
+		argvToLower += tolower(argv[1][x]);
+	}
 	
-	if(argvToLower != "cc" && argvToLower != "pc"){
+	if(argvToLower != "cc" && argvToLower != "pc") {
 		std::cout << "Invalid argument list.";
 	} else {
 		Game game(argvToLower[0]);
