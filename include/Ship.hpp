d9cac9ef5463cfcb04289adc09775157ed6704e3
Van Sang Ho <vansang.hocorp@gmail.com>
2023-01-18 12:44:57 +0100

    constructor fix

--- include/Ship.hpp
+++ include/Ship.hpp
@@ -9,21 +9,21 @@ Ship::Ship(const int size, Position bow, Position stern, char gridCharacter)
     : size_{size}, armor_{size}, gridCharacter_{gridCharacter} {
     //The only ship that has the same bow and stern positon is the submarine, which
     //does not have an orientation and the center position is the same as bow or stern position
-    if(bow == stern) {
+    if(bow == stern && size_ == 1) {
         center_ = bow;
         orientation_ = '-';
     //If the x coordinates of bow and stern are the same, initialize the 
     //center of the ship and set the orientation to horizontal
-    } else if(bow.getX() == stern.getX() && abs(bow.getY()-stern.getY())==size_-1) {
+    } else if(bow.getX() == stern.getX() && abs(bow.getY() - stern.getY()) == size_ - 1) {
         center_ = Position(bow.getX(), (bow.getY() + stern.getY()) / 2);
         orientation_ = 'H';
         
     //If the y coordinates of bow and stern are the same, initialize the 
     //center of the ship and set the orientation to vertical
-    } else if(bow.getY() == stern.getY() && abs(bow.getX() == stern.getX())==size_-1){
+    } else if(bow.getY() == stern.getY() && abs(bow.getX() - stern.getX()) == size_ - 1){
         center_ = Position((bow.getX() + stern.getX()) / 2, bow.getY());
         orientation_ = 'V';
-    } else throw std::invalid_argument("Invalid arguments for consturctor");
+    } else throw std::invalid_argument("Invalid arguments for constructor.");
 }
 
 #endif
