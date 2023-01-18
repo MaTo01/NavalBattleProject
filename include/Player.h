fb7f8e91a3a280e64367e38d32d35eb6b68272b6
VeronicaCis8 <118546929+VeronicaCis8@users.noreply.github.com>
2023-01-18 19:46:08 +0100

    aggiunti commenti

--- include/Player.h
+++ include/Player.h
@@ -11,16 +11,24 @@
 #include "SupportShip.h"
 #include "Submarine.h"
 
+//Parent class that will be inherited by the other two classes
+//that represent the two types of player
 class Player {
 protected:
+    //number of rows and columns for the attack and defense grid
     const int rows_;
     const int cols_;
+    //numeber of ship types that every player has
     const int nBattleships_;
     const int nSupportShips_;
     const int nSubmarines_;
+    //unique pointers to player's attack and defense grids
     std::unique_ptr<DefenseGrid> defenseGrid_;
     std::unique_ptr<AttackGrid> attackGrid_;
+    //pointer to player's enemy's defense grid
     DefenseGrid* enemyDefenseGrid_;
+    //output stream in which commands and the grids are
+    //going to be printed
     std::ofstream& fileOut_;
 
 public:
@@ -32,9 +40,14 @@ public:
     //Member functions
     DefenseGrid* getDefenseGrid() const { return defenseGrid_.get(); }
     void setEnemyDefenseGrid(DefenseGrid* enemyDefenseGrid) { enemyDefenseGrid_ = enemyDefenseGrid; }
-
+    //function to view player's grids
     void viewGrids(std::ostream& os);
+    //additional command to delete all the previous scans from the attack grid
+    //due to enemy moving his ships in their defense grid
     void clearAttackGridScans() { attackGrid_->clearScans(); }
+    /*function to know if player is the winner of the match
+    (player wins if they destroy every enemy ship, thus empting
+    their defense grid)*/
     bool isWinner() { return enemyDefenseGrid_->isGridEmpty(); }
 
     //Pure virtual function that represents the action of a 
