4af796a677120bdb42b90903c1b074e339887350
Van Sang Ho <vansang.hocorp@gmail.com>
2023-01-18 13:53:07 +0100

    Update NavalBattleReplay.cpp

--- src/NavalBattleReplay.cpp
+++ src/NavalBattleReplay.cpp
@@ -2,8 +2,19 @@
 #include <fstream>
 #include "Game.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+	
+	if(argc < 3 || argc > 4) {
+		std::cout << "Invalid argument list.\n";
+	} else {
+		std::string fileOut = "";
+		if(argc == 4) {
+			fileOut = std::string(argv[3]);
+		}
+		Game replay(*argv[1], argv[2], fileOut);
+		replay.setBattlefield();
+		replay.playReplay();
+	}
 
 	return 0;
 }
