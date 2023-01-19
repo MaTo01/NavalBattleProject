e1f1ae692bb87f7b74e35caa6b0157e2fe36bebc
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2023-01-19 17:35:50 +0100

    I need some space, pls

--- include/Player.hpp
+++ include/Player.hpp
@@ -20,7 +20,7 @@ Player::~Player() {
 void Player::viewGrids(std::ostream& os){
     os << "\t\t\t    Attack grid:" << std::endl;
     attackGrid_->printGrid(os);
-    os << "\t\t\t    Defense grid:" << std::endl;
+    os << "\n\t\t\t    Defense grid:" << std::endl;
     defenseGrid_->printGrid(os);   
 }
 
