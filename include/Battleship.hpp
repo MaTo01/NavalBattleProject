64b8d5ea102c7d2767496cc1a2f11c88e0a62aa6
Marco Toffoletto <92083229+MaTo01@users.noreply.github.com>
2023-01-19 18:36:28 +0100

    Update Battleship.hpp

--- include/Battleship.hpp
+++ include/Battleship.hpp
@@ -6,21 +6,17 @@
 
 /*
 The action performed by the battleship is to fire at a certain position on the enemy's defense grid:
-first it checks if the position where the action is requested has not already been hit and, if so, fires.
+The battleship fires even if a position has already been marked. this must be done because an enemy support ship
+could heal a damaged ship, if there was a check I could no longer sink the hit and then healed ship.
 This is possible thanks to the "isShipAtPosition" function which checks if a ship is present in that position,
 and marks the shot on the attack grid and enemy's defense grid accordingly.
-On the other hand, if the position where the action is requested has already been hit, it throws an exception.
 */
 void Battleship::action(Position pos, DefenseGrid* enemyDefenseGrid) { 
-    if(!attackGrid_->isAlreadyMarked(pos)) {
-        if(enemyDefenseGrid->isShipAtPosition(pos)) {
-            attackGrid_->markHit(pos);
-            enemyDefenseGrid->markShipAsHit(pos);
-        } else {
-            attackGrid_->markMiss(pos);
-        }
+    if(enemyDefenseGrid->isShipAtPosition(pos)) {
+        attackGrid_->markHit(pos);
+        enemyDefenseGrid->markShipAsHit(pos);
     } else {
-        throw std::invalid_argument("Cell already marked as hit.");
+        attackGrid_->markMiss(pos);
     }
 }
 
